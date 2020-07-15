86fe7de39b9d37ff6122fd1d9e5b94088d2bdafa
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-07-15 12:18:17 -0500

    implements fetch projects route

--- fetch_projects/__init__.py
+++ fetch_projects/__init__.py
@@ -1,23 +1,68 @@
 import requests as req
 import json
 
-def map_projects(repo):
-    print(repo)
-    if not repo.get('fork'):
-        return dict(
-            id=repo['id'],
-            name=repo['name']
-        )
+from utils import get_prop
+
+def map_topics(topic: dict):
+    return get_prop(['topic', 'name'], topic)
+
+
+def map_projects(repo: dict):
+    language = get_prop(['primaryLanguage', 'name'], repo)
+    topics = list(map(
+        map_topics,
+        get_prop(['repositoryTopics', 'nodes'], repo)
+    ))
+
+    return dict(
+        id = repo.get('id'),
+        name = repo.get('name'),
+        publlished_on = dict(github = repo.get('url')),
+        language = language,
+        topics = topics,
+        updated_at = repo.get('updated_at')
+    )
 
 def fetch_projects():
-    user = 'naijopkr'
-    url = f'https://api.github.com/users/{user}/repos?type=owner&sort=updated&per_page=100'
+    token = '3f3b718fe7b4ab69451836d7b8fac85023afc19c'
+    url = 'https://api.github.com/graphql'
+    query = """query {
+    viewer {
+        repositories (first: 100, orderBy: { field: CREATED_AT, direction: DESC }) {
+        nodes {
+            databaseId
+            id
+            name
+            description
+            url
+            updatedAt
+            primaryLanguage {
+            name
+            }
+            repositoryTopics(first: 5) {
+            nodes {
+                topic {
+                name
+                }
+            }
+            }
+        }
+        }
+    }
+    }"""
 
-    try:
-        res = req.get(url)
-        projects = json.loads(res.text)
-        repos = filter(lambda x:x, map(map_projects, projects))
-        return list(repos)
-    except Exception as e:
-        print(e)
+    res = req.post(
+        url,
+        json={'query': query},
+        headers={'Authorization': f'bearer {token}'}
+    )
+
+    projects_list = get_prop(
+        ['data', 'viewer', 'repositories', 'nodes'],
+        json.loads(res.text)
+    )
+
+    if not projects_list:
         return []
+
+    return list(map(map_projects, projects_list))
